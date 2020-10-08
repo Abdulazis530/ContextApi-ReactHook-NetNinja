81aafdea7f4f1745e4d223c6a811fd99e42df3fb
azis <abdlazis530@gmail.com>
2020-10-08 17:23:33 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Booklists from './components/BookLists';
+import Navbar from './components/Navbar';
+import ThemeToggle from './components/ThemeToggle';
+import ThemeContextProvider from './context/ThemeContext';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+      <ThemeContextProvider>
+        <Navbar />
+        <Booklists />
+        <ThemeToggle />
+      </ThemeContextProvider>
     </div>
-  );
+  )
 }
-
-export default App;
